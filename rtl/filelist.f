671141c5e56edb8fc3c1f35621c041dfe05f72f8
Ziyu Huang <ziyu4huang@gmail.com>
2025-02-25 07:03:57 +0800

    add run_verilator.sh

---
+++ rtl/filelist.f
@@ -0,0 +1,3 @@
+./viterbi_tx_rx.v 
+./original/mem_8x1024_ori.v 
+./original/mem_1x1024_ori.v
---
+++ rtl/run_verilator.sh
@@ -0,0 +1,1 @@
+verilator --xml-only --timing -Wno-IMPLICIT -Wno-WIDTHEXPAND -Wno-WIDTHTRUNC -I./lib -f filelist.f
