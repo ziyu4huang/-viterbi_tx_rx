ef692021736f63a65309dd3a609089b2ac1172bf
Ziyu Huang <ziyu4huang@gmail.com>
2025-02-25 07:22:24 +0800

    mv run_verilog.sh to top

---
+++ filelist.f
@@ -0,0 +1,3 @@
+./rtl/viterbi_tx_rx.v 
+./rtl/original/mem_8x1024_ori.v 
+./rtl/original/mem_1x1024_ori.v
--- rtl/filelist.f
+++
@@ -1,3 +0,0 @@
-./viterbi_tx_rx.v 
-./original/mem_8x1024_ori.v 
-./original/mem_1x1024_ori.v
